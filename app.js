525f30d3e1825360581dfb86eea260a8b166a836
nhutthuong <nhutthuonga3ltt@gmail.com>
2026-09-16 16:06:16 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const contactsRouter = require("./app/routes/contact.route");
-
+const ApiError = require("./app/api-error");
 const app = express();
 
 app.use(cors());
@@ -15,5 +15,17 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactsRouter);
+
+// Handle 404 response
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+// Error-handling middleware
+app.use((error, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;
